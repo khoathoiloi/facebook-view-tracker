71638302fd833635916574dd32245ffb4083a429
khoathoiloi <321614387+khoathoiloi@users.noreply.github.com>
2026-08-27 13:57:32 +0700

    Update app launcher and dependencies

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import uvicorn
 import webbrowser
 import threading
 import time
+from app.main import app
 
 def open_browser():
     time.sleep(1.5)
@@ -13,4 +14,4 @@ if __name__ == "__main__":
     print("   Website: http://127.0.0.1:8000")
     print("==================================================================")
     threading.Thread(target=open_browser, daemon=True).start()
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
